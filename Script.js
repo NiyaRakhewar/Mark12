c82efea81be4eb42ed9c4d9c0a173cf813e55ee4
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-12 17:12:06 +0530

    update

---
+++ Script.js
@@ -0,0 +1,4 @@
+const inputs = document.querySelectorAll("#angle-input");
+console.log(inputs);
+// const isTriangleButton = document.querySelector("is-triangle-button");
+// const output = document.querySelectorAll("#output");
