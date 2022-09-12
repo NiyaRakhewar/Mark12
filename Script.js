2b934bbb6c7a428deb7b406672f3901ce0437821
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-12 19:42:28 +0530

    update

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
-const inputs = document.querySelectorAll("#angle-input");
+const inputs = document.querySelectorAll(".angle-input");
 console.log(inputs);
-// const isTriangleButton = document.querySelector("is-triangle-button");
-// const output = document.querySelectorAll("#output");
+
+const isTriangleButton = document.querySelector("#is-triangle-button");
+const output = document.querySelectorAll("#output");
